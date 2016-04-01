d92bbeab8665c46b6c5ab04e20ed840dacab4063
Cheng Fei <cheng.fei1026@gmail.com>
2016-04-01 23:47:18 +0800

    Update cond_sig_either.c

--- src/cond_sig_either.c
+++ src/cond_sig_either.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <pthread.h>
 
-
+/*
 int pthread_mutex_init(pthread_mutex_t *mutex, 
                        const pthread_mutexattr_t *attr)        ;
 int pthread_mutex_lock(pthread_mutex_t *mutex)                   ;
 int pthread_mutex_unlock(pthread_mutex_t *mutex)                     ;
 int pthread_mutex_trylock(pthread_mutex_t *mutex)                 ;
 int pthread_mutexattr_destroy(pthread_mutexattr_t *mattr)             ;
+*/
 
 int  flag;
 pthread_cond_t cond;
